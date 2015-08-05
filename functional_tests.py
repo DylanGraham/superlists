9b16c8e6216efef245b6bdebdcf3f8cb94674756
Dylan Graham <dylan.graham@gmail.com>
2015-08-05 15:45:44 +1000

    More tests

--- functional_tests.py
+++ functional_tests.py
@@ -24,8 +24,8 @@ class NewVisitorTest(unittest.TestCase):
         # Can enter a to-do item right away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
 
         # Types 'Buy milk'
@@ -35,25 +35,27 @@ class NewVisitorTest(unittest.TestCase):
         # '1: Buy milk' as an item on a to-do list
         inputbox.send_keys(Keys.ENTER)
 
-        #import time
-        #time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_id('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy milk' for row in rows),
-                "New to-do item did not appear in table"
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy milk', [row.text for row in rows])
 
         # Text box is offering to add another line.
         # User enters 'Bake cake'
-        self.fail('Finish this test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Bake cake')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates and shows both items
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy milk', [row.text for row in rows])
+        self.assertIn('2: Bake cake', [row.text for row in rows])
 
         # The site has made a unique URL for the list
         # and lets the user know.
 
         # User goes to the URL, the list is there
+        self.fail('Finish this test!')
 
 if __name__ == '__main__':
     unittest.main()
